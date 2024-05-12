be3348ecff002f4f970edae75a04c279cc989397
NikhilSuryanarayanan <95303267+NikhilSuryanarayanan@users.noreply.github.com>
2024-05-12 16:25:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000; // You can choose any available port
 
 // Sample route
 app.get('/', (req, res) => {
-  res.send('Hello, daniel');
+  res.send('Hello, jerry');
 });
 
 // Starting the server
